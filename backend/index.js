26df69534b990455a1eb23c70a242ef9a8c9b2d6
Igor Abratkiewicz <igi5786@gmail.com>
2023-11-21 17:50:27 +0000

    init backend

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const httpServer = express();
+const dialer = require('dialer').Dialer;
+
+const config = {
+    url: 'https://uni-call.fcc-online.pl',
+    login: 'focus05',
+    password: '#hw4tredbw4n'
+};
+
+dialer.configure(config);
+
+// Serwer nasłuchuje na porcie 3000
+httpServer.listen(3000, function () {
+ console.log('Example app listening on port 3000!')
+})
+
+
+// Definiowanie odpowiedzi na request get
+httpServer.get('/call/:number1/:number2', (req, res) => {
+    const number1 = req.params.number1;
+    const number2 = req.params.number2;
+    dialer.call(number1,number2);  
+    res.json({success:true});
+})
